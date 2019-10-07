3984e08fd01b72c625c52db6ab47368cb8b92b3c
Marc <marcchai123@gmail.com>
2019-10-07 21:41:47 +0800

    created table listing
    added simple layout
    added numbers to tables

---
+++ tablelisting.php
@@ -0,0 +1,47 @@
+<!DCOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Food Order Cart</title>
+        <!--Required meta tags-->
+        <meta charset="utf-8"/>
+        <meta name="viewport" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+        <!--Bootstrap CSS-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+        
+        
+        
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+               <?php 
+                $i=1;
+                $tables = 15;
+                for($i=1; $i<=$tables; $i++){
+               ?>
+                <div class="col-md-2 border p-3 m-3">
+                   <?php echo $i; ?>
+                </div>
+                <?php } ?>
+            </div>
+        </div>
+        
+        
+        
+        
+        <!--Bootstrap JS-->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script>
+            $('.table tbody').on('click','.btn',function(){
+                $(this).closest('tr').remove(); //removes the closest table row, in this case, the table row where the delete button is pressed
+            });
+            
+            $('#resetBtn').on('click',function(){
+               $('#table tbody').empty(); //empties the table
+                $('#totalAmt').text("RM 0");
+            });
+        </script>
+    </body>
+</html>
