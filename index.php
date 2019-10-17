e5480f84a066f1db3ee966c5982a9f2ee74964e9
TeradonGenesis <48543482+TeradonGenesis@users.noreply.github.com>
2019-10-17 21:35:55 +0800

    Lim Jia Lok updated the front page of the sotware

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 
                 <div class="row mt-5">
-                    <div class="col-12 col-sm-12 col-md-12 col-col-lg-12">
+                    <div class="col-12 col-sm-12 col-md-12 col-col-lg-12 mb-4">
 
                         <h1 class="text-center" id="title">Please Select An Interface</h1>
                     </div>
@@ -25,61 +25,61 @@
 
                     <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center ">
 
-                        <div class="card english">
-                            <a class="img-card" href=#>
-                                <img src="images/englishprofile.png" alt="subjects" />
-                            </a>
-                            <div class="card-content">
-                                <h4 class="card-title text-center">
-                                    <a href=#> ENGLISH
-                                    </a>
-                                </h4>
+                        <div class="card menu">
+                            <div class="img-card mb-3">
+                                <img src="images/menu.png" alt="menu" />
                             </div>
-                            <div class="card-read-more big-button-english">
-                                <a href=# class="btn btn-link btn-block">
-                                    EXPLORE
+                            <div class="card-content mb-3">
+                                <div class="card-title text-center display-3">
+                                   Menu
+
+                                </div>
+                            </div>
+                            <div class="card-read-more big-button-menu">
+                                <a href="foodordercart.php" class="btn btn-block stretched-link text-white font-weight-bold">
+                                    For waiters
                                 </a>
                             </div>
                         </div>
 
                     </div>
-                    
+
                     <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center ">
 
-                        <div class="card english">
-                            <a class="img-card" href=#>
-                                <img src="images/menu.jpg" alt="subjects" />
-                            </a>
-                            <div class="card-content">
-                                <h4 class="card-title text-center">
-                                    <a href=#> ENGLISH
-                                    </a>
-                                </h4>
+                        <div class="card menu">
+                            <div class="img-card mb-3">
+                                <img src="images/menu.png" alt="cashier" />
                             </div>
-                            <div class="card-read-more big-button-english">
-                                <a href=# class="btn btn-link btn-block">
-                                    EXPLORE
+                            <div class="card-content mb-3">
+                                <div class="card-title text-center display-3">
+                                    Payment
+
+                                </div>
+                            </div>
+                            <div class="card-read-more big-button-menu">
+                                <a href="tablelisting.php" class="btn btn-block stretched-link text-white font-weight-bold">
+                                    For cashiers
                                 </a>
                             </div>
                         </div>
 
                     </div>
-                    
+
                     <div class="col-12 col-sm-4 col-md-4 col-lg-4 text-center ">
 
-                        <div class="card english">
-                            <a class="img-card" href=#>
-                                <img src="images/englishprofile.png" alt="subjects" />
-                            </a>
-                            <div class="card-content">
-                                <h4 class="card-title text-center">
-                                    <a href=#> ENGLISH
-                                    </a>
-                                </h4>
+                        <div class="card menu">
+                            <div class="img-card mb-3">
+                                <img src="images/menu.png" alt="kitchen" />
                             </div>
-                            <div class="card-read-more big-button-english">
-                                <a href=# class="btn btn-link btn-block">
-                                    EXPLORE
+                            <div class="card-content mb-3">
+                                <div class="card-title text-center display-3">
+                                    Kitchen Inbox
+
+                                </div>
+                            </div>
+                            <div class="card-read-more big-button-menu">
+                                <a href="kitcheninbox.php" class="btn btn-block stretched-link text-white font-weight-bold">
+                                    For kitchen staff
                                 </a>
                             </div>
                         </div>
