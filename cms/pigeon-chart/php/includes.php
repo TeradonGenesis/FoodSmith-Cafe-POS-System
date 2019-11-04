bb4687cf5e7df716f4f767e3b08d05bf3298f034
choo98 <48203082+choo98@users.noreply.github.com>
2019-11-04 21:11:46 +0800

    Aldalton set up pigeon chart
    Aldalton set up the pigeon chart for displaying transactions/reports

---
+++ cms/pigeon-chart/php/includes.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+    <script src='pigeon-chart/js/angular.min.1.6.4.js'></script>
+	<script src='pigeon-chart/js/underscore-min.js'></script>
+	<script src='pigeon-chart/js/highcharts.js'></script>
+	<script src='pigeon-chart/js/exporting.js'></script>
+	<script src='pigeon-chart/js/series-label.js'></script>
+	<script src='pigeon-chart/js/grouped-categories.js'></script>
+    <script src='pigeon-chart/js/pigeon-chart.js'></script>";
+?>
