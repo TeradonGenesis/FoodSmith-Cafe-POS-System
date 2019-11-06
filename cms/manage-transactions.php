96fabf2af162e221f77c85f12f3654388357cb35
diggledoot <39117755+diggledoot@users.noreply.github.com>
2019-11-06 21:16:35 +0800

    DARREN 100085926
    Implement full sort function for day or month

--- cms/manage-transactions.php
+++ cms/manage-transactions.php
@@ -19,24 +19,25 @@
             <?php include 'nav.php'?>
 
             <form method="post" action="export.php">
-                <input type="submit" name="export" value="CSV Export" class="btn btn-success"/>
+                <input type="submit" name="export" value="CSV Export" class="btn btn-success" />
             </form>
-            
-            
+
+
             <div class="table-container">
                 <div class="row mt-5">
-                    <div class="col-md-6">
-                        <h5>Search By Day of Month</h5>
+                    <div class="col-md-6 mb-2">
+                        <h5>Search by Day or Month</h5>
                         <div class="input-group">
-                            <input type="text" name="day" id="day" class="form-control mb-5">
+                            <input type="text" name="day" id="day" placeholder="Day" class="form-control">
+                            <input type="text" name="month" id="month" placeholder="Month" class="form-control ml-2 mb-2">
                             <span class="input-group-btn">
                                 <button id="search" class="btn btn-success ml-2">SEARCH</button>
                                 <button id="reset" class="btn btn-danger ml-2">RESET</button>
                             </span>
                         </div>
                     </div>
-                    
-                
+
+
                     <div class="col-md-12 tab-pane active" id="display">
                         <table class="table table-borded table-striped" id="transactionTable">
                             <thead class="thead-dark">
@@ -68,35 +69,38 @@
     <?php include 'footer.php'?>
     <script src="../js/manage-category.js"></script>
     <script>
-        $('#search').on('click',function(){
+        $('#search').on('click', function() {
             $day = $('#day').val();
-            //month
-            $('#transactionTable tbody').empty();
-            $.ajax({
-                type:'POST',
-                url:'../connection/retrieve.php',
-                data:{
-                    day:$day
-                    //month
-                },
-                dataType:'html',
-                success:function(data){
-                    var result=data
-                    $('#transactionTable tbody').append(result);
-                }
-            });
-            
-            
+            $month = $('#month').val()
+            if($day=='' && $month==''){
+                alert("Enter a value in either day or month field to conduct search!");
+            }else{
+                $('#transactionTable tbody').empty();
+                $.ajax({
+                    type: 'POST',
+                    url: '../connection/retrieve.php',
+                    data: {
+                        day: $day,
+                        month: $month
+                    },
+                    dataType: 'html',
+                    success: function(data) {
+                        var result = data
+                        $('#transactionTable tbody').append(result);
+                    }
+                });
+            }
         })
-        
-        $('#reset').on('click',function(){
+
+        $('#reset').on('click', function() {
             $('#day').val('');
+            $('#month').val('');
             $('#transactionTable tbody').empty();
             $.ajax({
-                url:'../connection/reset.php',
-                dataType:'html',
-                success:function(data){
-                    var result=data
+                url: '../connection/reset.php',
+                dataType: 'html',
+                success: function(data) {
+                    var result = data
                     $('#transactionTable tbody').append(result);
                 }
             })
