6f62ea354f272b8ffc5230e01c4c7ba7c582b462
diggledoot <39117755+diggledoot@users.noreply.github.com>
2019-11-13 20:38:09 +0800

    DARREN 100085926
    Adapted sidepanel from CMS to sidepanel.inc

--- includes/sidepanel.inc.php
+++ includes/sidepanel.inc.php
@@ -1,35 +1,38 @@
 <!-- Sidebar  -->
-        <nav id="sidebar">
-            <div class="sidebar-header">
-                <h3>Foodsmith</h3>
-                <strong>AP</strong>
-            </div>
+<?php 
+    $activePage = basename($_SERVER['PHP_SELF'], ".php");
+?>
+<nav id="sidebar">
+    <div class="sidebar-header">
+        <h3>FoodSmith</h3>
+        <strong>AP</strong>
+    </div>
 
-            <ul class="list-unstyled components">
-                <li class="active">
-                    <a href="index.php">
-                        <i class="fas fa-home"></i>
-                        Home
-                    </a>
-                </li>
-                <li>
-                    <a href="foodordercart.php">
-                        <i class="fas fa-utensils"></i>
-                        Menu
-                    </a>
-                </li>
-                <li>
-                    <a href="tablelisting.php">
-                        <i class="fas fa-cash-register"></i>
-                        Payment
-                    </a>
-                </li>
-                <li>
-                    <a href="kitcheninbox2.php">
-                        <i class="fas fa-hamburger"></i>
-                        Kitchen
-                    </a>
-                </li>
-            </ul>
+    <ul class="list-unstyled components">
+        <li class="<?= ($activePage == 'index') ? 'active':''; ?>">
+            <a href="index.php">
+                <i class="fas fa-home"></i>
+                Home
+            </a>
+        </li>
+        <li class="<?= ($activePage == 'foodordercart') ? 'active':''; ?>">
+            <a href="foodordercart.php">
+                <i class="fas fa-utensils"></i>
+                Menu
+            </a>
+        </li>
+        <li class="<?= ($activePage == 'tablelisting') ? 'active':''; ?>">
+            <a href="tablelisting.php">
+                <i class="fas fa-cash-register"></i>
+                Payment
+            </a>
+        </li>
+        <li class="<?= ($activePage == 'kitcheninbox2') ? 'active':''; ?>">
+            <a href="kitcheninbox2.php">
+                <i class="fas fa-hamburger"></i>
+                Kitchen
+            </a>
+        </li>
+    </ul>
 
-        </nav>
+</nav>
